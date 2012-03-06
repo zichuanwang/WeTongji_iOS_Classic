8c355781a7c4886e5cf49c1668cc5d59c07b2ef0
bluebitch <wzc345@gmail.com>
2012-03-06 15:28:24 +0800

    Initial Commit

---
+++ WeTongji/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WeTongji
+//
+//  Created by 紫川 王 on 12-3-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ WeTongji/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeTongji
+//
+//  Created by 紫川 王 on 12-3-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
