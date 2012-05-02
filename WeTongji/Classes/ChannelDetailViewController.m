ba5dccc438f03955eb72ee5571fdf159b67d7a55
bluebitch <wzc345@gmail.com>
2012-05-02 21:12:42 +0800

    ～

--- WeTongji/Classes/ChannelDetailViewController.m
+++ WeTongji/Classes/ChannelDetailViewController.m
@@ -3,7 +3,7 @@
 //  WeTongji
 //
 //  Created by 紫川 王 on 12-4-26.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012年 Tongji Apple Club. All rights reserved.
 //
 
 #import "ChannelDetailViewController.h"
